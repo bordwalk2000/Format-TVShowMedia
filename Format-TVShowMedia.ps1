7014ac969764ae0788d97441593e8b5134fc4183
Bradley Herbst <Bordwalk2000@gmail.com>
2017-10-03 12:58:12 -0500

    Added function for getting episode name & number of seasons
    Moved getting tv shows episode’s name to a function.  Also created a function for getting the tv shows season.  Also created a loop to get all the tv show episode names for every season.

--- Format-TVShowMedia.ps1
+++ Format-TVShowMedia.ps1
@@ -1,29 +1,50 @@
-﻿$url = ''
-$HTML = Invoke-WebRequest -Uri $URL -ErrorAction Stop
+﻿function Get-IMDBTVShowSeasonEpisodes { 
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory,
+               ValueFromPipeline,
+               ValueFromPipelineByPropertyName)]
+        [string] $url
+    )
 
-$results =  $HTML.ParsedHtml.body.getElementsByTagName("div") | Where {$_.classname -like '*list_item*'}
+    $HTML = Invoke-WebRequest -Uri $URL -ErrorAction Stop
+    $results =  $HTML.ParsedHtml.body.getElementsByTagName("div") | Where {$_.classname -like '*list_item*'}
 
-foreach ($episode in $results) {
-    $Season_episode = (($episode.childNodes[1].childNodes[1].textContent).Trim()) -split ','
-    
-    
-    #Get Season
-    if([int]$Season_episode[0].SubString(1) -le 9) {
-        $Season = 'S0' + $Season_episode[0].SubString(1)
-    } else {
-        $season = $Season_episode[0].ToUpper()
+    foreach ($episode in $results) {
+        $Season_episode = (($episode.childNodes[1].childNodes[1].textContent).Trim()) -split ','
+        $EpisodeName = 'S{0:D2}' -f [int]$Season_episode[0].SubString(1) + '.E{0:D2}' -f [int]$Season_episode[1].Trim().SubString(2) + ' ' + ($episode.childNodes[3].childNodes[5].innerText).Trim()
+        
+        Write-Output $EpisodeName 
     }
-    
-    #Get Episode
-    if([int]$Season_episode[1].Trim().SubString(2) -le 9) {
-        $episodenumber = '.E0' + $Season_episode[1].Trim().SubString(2)
-    } else {
-        $episodenumber = '.E' + $Season_episode[1].Trim().SubString(2)
-    }
-    
-
-    $season + $episodenumber + ' ' + ($episode.childNodes[3].childNodes[5].innerText).Trim()
 }
 
 
-#load_next_episodes
+function Get-IMDBTVShowSeasons { 
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory,
+               ValueFromPipeline,
+               ValueFromPipelineByPropertyName)]
+        [string] $url
+    )
+
+    $HTML = Invoke-WebRequest -Uri $URL -ErrorAction Stop
+    $results =  $HTML.ParsedHtml.body.getElementsByTagName("div") | Where {$_.classname -like '*seasons-and-year-nav*'}
+    
+    foreach ($season in $results.childNodes[7].innerHTML.trim() -Split(“`n”)) {
+        $props = @{
+            'Season' = (($season -split '>')[1]).Substring(0,1);
+            'URL' = 'http://www.imdb.com/' +($season.TrimStart('<a href="/') -split ';')[0];
+        }
+
+        New-Object -TypeName PSObject -Property $props
+    }
+}
+
+
+Get-IMDBTVShowSeasons -url '' | Sort Season | 
+% {
+ 
+    Get-IMDBTVShowSeasonEpisodes -url $_.url
+ 
+ }
