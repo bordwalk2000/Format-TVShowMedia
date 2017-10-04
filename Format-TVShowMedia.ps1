4c0f27d33fb196f8fcf9f922c9526cb3a4c29d38
Bradley Herbst <Bordwalk2000@gmail.com>
2017-10-03 23:48:22 -0500

    Parametrizing PowerShell Script
    Worked on parametrizing the PowerShell script file to become a tool.

--- Format-TVShowMedia.ps1
+++ Format-TVShowMedia.ps1
@@ -1,10 +1,37 @@
-﻿function Get-IMDBTVShowSeasonEpisodes { 
+﻿<#
+.SYNOPSIS
+ 
+.DESCRIPTION
+
+ 
+.PARAMETER FolderPath
+Specifiy the path to the folder where the files are located that need to be renamed and organised.
+ 
+.PARAMETER URL
+
+.EXAMPLE
+
+.NOTES
+    Author: Bradley Herbst
+    Version: 0.1
+    Last Updated: October 3, 2017
+        
+    ChangeLog
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)][string] $FolderPath,
+    [string] $url
+)
+
+
+    BEGIN {
+
+function Get-IMDBTVShowSeasonEpisodes { 
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory,
-               ValueFromPipeline,
-               ValueFromPipelineByPropertyName)]
-        [string] $url
+        [Parameter(Mandatory)][string] $url
     )
 
     $HTML = Invoke-WebRequest -Uri $URL -ErrorAction Stop
@@ -22,10 +49,7 @@
 function Get-IMDBTVShowSeasons { 
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory,
-               ValueFromPipeline,
-               ValueFromPipelineByPropertyName)]
-        [string] $url
+        [Parameter(Mandatory)][string] $url
     )
 
     $HTML = Invoke-WebRequest -Uri $URL -ErrorAction Stop
@@ -34,17 +58,54 @@ function Get-IMDBTVShowSeasons {
     foreach ($season in $results.childNodes[7].innerHTML.trim() -Split(“`n”)) {
         $props = @{
             'Season' = (($season -split '>')[1]).Substring(0,1);
-            'URL' = 'http://www.imdb.com/' +($season.TrimStart('<a href="/') -split ';')[0];
+            'URL' = 'http://www.imdb.com/' + ($season.TrimStart('<a href="/') -split ';')[0];
         }
 
         New-Object -TypeName PSObject -Property $props
     }
 }
 
+    }
 
-Get-IMDBTVShowSeasons -url '' | Sort Season | 
+    PROCESS {
+    
+If (!$url) {
+    #Remove parents folders from the Folder path, then stores the working folder in its own variable.
+    $FolderName = Split-Path $FolderPath -leaf
+
+    #Replaces the spaces in the string to + signs to be used in the search query
+    $SearchString = $FolderName.replace(' ','+')
+
+    #Places the SerachString into the Search URL to be Used
+    $SearchQuery = "http://www.imdb.com/find?ref_=nv_sr_fn&q=$SearchString&s=all"
+
+    #Search for the course name and pulls the top result.
+    $HTML = Invoke-WebRequest -Uri $SearchQuery 
+    $results = $HTML.ParsedHtml.body.getElementsByTagName("table") | Where{$_.classname -eq 'findList'} | 
+    foreach{$_.getElementsByTagName("tr") | Where{$_.classname -like '*findResult*'}} | Select -First 1
+
+    $url = 'http://www.imdb.com/' + (($results.innerHTML -split '<td class="result_text">')[1].TrimStart('<a href="/') -split '"')[0]
+
+}
+
+
+Get-IMDBTVShowSeasons -url $url | Sort Season | 
 % {
+    #Create Season Folder if it doesn't exisist
+    if(!(Test-Path -Path ("$FolderPath\Season {0:D2}" -f ([int]$_.Season)))){New-Item -ItemType directory -Path ("$FolderPath\Season {0:D2}" -f ([int]$_.Season))}
+    
+    Get-IMDBTVShowSeasonEpisodes -url $_.url | 
+    % {
+       #Looks for files in the $FoldePath Directory and sub directories that are the file extension spcified in the params.
+        Get-ChildItem -Path $FolderPath -Filter "*" -Recurse | 
+        #$TitleName -match "S(?<season>\d{1,2}).(\s*)E(?<episode>\d{1,2})|Season(\d{1,2}).(\s*)Episode(\d{1,2})"
+    }
+}
  
-    Get-IMDBTVShowSeasonEpisodes -url $_.url
- 
- }
+#Looks for folders with no files in them and then deletes the files if any were found.
+Get-ChildItem -Path $FolderPath -recurse | Where {$_.PSIsContainer -and @(Get-ChildItem -Lit $_.Fullname -r | 
+Where {!$_.PSIsContainer}).Length -eq 0} |
+Remove-Item -recurse
+
+    }
+
